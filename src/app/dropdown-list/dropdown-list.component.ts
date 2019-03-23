00bf7cd54d0b39ba1d5ddf46b62c2a4e2656c173
Taras_Kovalev <taraswww777@mail.ru>
2019-03-23 21:53:04 +0300

    started develop dropdown list

---
+++ src/app/dropdown-list/dropdown-list.component.ts
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+import { Component, Input, OnInit } from '@angular/core';
+// app
+import { DEFAULT_PATH_TO_ID, DEFAULT_PATH_TO_TITLE, DropdownListOptions } from './contract';
+
+@Component({
+  selector: 'app-dropdown-list',
+  templateUrl: './dropdown-list.component.html',
+  styleUrls: ['./dropdown-list.component.css'],
+})
+export class DropdownListComponent implements OnInit {
+  items: object[] = [];
+
+  private pathToID: string = DEFAULT_PATH_TO_ID;
+  private pathToTitle: string = DEFAULT_PATH_TO_TITLE;
+
+  constructor() {}
+
+  @Input()
+  set options(options: DropdownListOptions<any>) {
+    this.items = options.items;
+    this.pathToID = options.pathToID || DEFAULT_PATH_TO_ID;
+    this.pathToTitle = options.pathToTitle || DEFAULT_PATH_TO_TITLE;
+  }
+
+  getItemTitle(item: object): string {
+    return _.get(item, this.pathToTitle);
+  }
+
+  getItemID(item: object): string {
+    return _.get(item, this.pathToID);
+  }
+
+  ngOnInit() {
+  }
+
+}
