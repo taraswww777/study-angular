23238886d65e02f9a48d3a51468e8bb53a18981a
Taras_Kovalev <taraswww777@mail.ru>
2019-03-23 22:30:06 +0300

    register dropdown-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DropdownListComponent } from './dropdown-list/dropdown-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
