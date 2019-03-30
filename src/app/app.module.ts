cdd9a8fe719ec5848bd3a2e8b0794ba7081179bb
Taras_Kovalev <taraswww777@mail.ru>
2019-03-30 18:17:30 +0300

    Применены обновлёные правила линтинга

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,15 @@ import { AppComponent } from './app.component';
 import { DropdownListComponent } from './dropdown-list/dropdown-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DropdownListComponent,
-  ],
-  imports: [
-    BrowserModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [
+		AppComponent,
+		DropdownListComponent,
+	],
+	imports: [
+		BrowserModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {
 }
